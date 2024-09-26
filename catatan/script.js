29560ddc11c11c74aba83f9c4135e4aacf570c78
unknown <bgsdewoo20@gmail.com>
2024-09-26 09:34:59 +0700

    Callback Hell Di Javascript Bikin Serem Dibacanya

--- catatan/script.js
+++ catatan/script.js
@@ -16,3 +16,24 @@
 //   );
 // }, 3000);
 // console.log("Bakal muncul kedua");
+
+//Topic Tiga Callback Hell Di Javascript Bikin Serem Dibacanya
+// setTimeout(() => {
+//   document.body.style.backgroundColor = "red";
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "blue";
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = "green";
+//       setTimeout(() => {
+//         document.body.style.backgroundColor = "yellow";
+//         setTimeout(() => {
+//           document.body.style.backgroundColor = "purple";
+//           setTimeout(() => {
+//             document.body.style.backgroundColor = "orange";
+//             // Tambahkan lebih banyak warna atau loop lagi jika ingin
+//           }, 1000);
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
