62017f863ffdc1bda7af0957dfb20fd2efa31a02
unknown <bgsdewoo20@gmail.com>
2024-09-26 10:32:42 +0700

    Latihan Request Pakai Callback Function

--- catatan/script.js
+++ catatan/script.js
@@ -37,3 +37,76 @@
 //     }, 1000);
 //   }, 1000);
 // }, 1000);
+
+//Topic Empat Latihan Request Pakai Callback Function
+// callback version
+// const requestCallback = (url, success, failure) => {
+//   const delay = Math.floor(Math.random() * 4500) + 500;
+//   setTimeout(() => {
+//     if (delay > 4000) {
+//       failure("Error: Connection Timeout");
+//     } else {
+//       success(`Success: ${url} (${delay}ms)`);
+//     }
+//   }, delay);
+// };
+
+// // Promise version
+// const requestPromise = (url) => {
+//   return new Promise((resolve, reject) => {
+//     const delay = Math.floor(Math.random() * 4500) + 500;
+//     setTimeout(() => {
+//       if (delay > 2000) {
+//         reject("Error: Connection Timeout");
+//       } else {
+//         resolve(`Success: ${url} (${delay}ms)`);
+//       }
+//     }, delay);
+//   });
+// };
+
+// requestCallback(
+//   "movie.com",
+//   function (response) {
+//     console.log("It worked", response);
+//     requestCallback(
+//       "movie.com/actors",
+//       function (response) {
+//         console.log("It worked", response);
+//         requestCallback(
+//           "movie.com/actors/details",
+//           function (response) {
+//             console.log("It worked", response);
+//             requestCallback(
+//               "movie.com/actors/details/movies",
+//               function (response) {
+//                 console.log("It worked", response);
+//                 requestCallback(
+//                   "movie.com/actors/details/movies/reviews",
+//                   function (response) {
+//                     console.log("It worked", response);
+//                   },
+//                   function (error) {
+//                     console.log("Error", error);
+//                   }
+//                 );
+//               },
+//               function (error) {
+//                 console.log("Error", error);
+//               }
+//             );
+//           },
+//           function (error) {
+//             console.log("Error", error);
+//           }
+//         );
+//       },
+//       function (error) {
+//         console.log("Error", error);
+//       }
+//     );
+//   },
+//   function (error) {
+//     console.log("Error", error);
+//   }
+// );
