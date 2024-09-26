02ec38746e7a12ff75faec0ecda895f364c8726b
unknown <bgsdewoo20@gmail.com>
2024-09-26 09:01:53 +0700

    Satu Mengenal Call Stack Function

---
+++ catatan/script.js
@@ -0,0 +1,9 @@
+//Topic Satu Mengenal Call Stack Function
+// const perkalian = (x, y) => x * y;
+// const akar = (x) => perkalian(x, x);
+
+// const pythagoras = (a, b, c) => {
+//   return akar(a) + akar(b) == akar(c);
+// };
+
+// console.log(pythagoras(3, 4, 5));
