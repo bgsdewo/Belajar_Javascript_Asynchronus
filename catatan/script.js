a3fd3a6aa2d5667b2acc8e8b1bbdcad34ff53514
unknown <bgsdewoo20@gmail.com>
2024-09-26 10:59:49 +0700

    Cara Benar Menggunakan Fungsi Promise

--- catatan/script.js
+++ catatan/script.js
@@ -151,3 +151,36 @@
 //   .catch((error) => {
 //     console.log("error", error);
 //   });
+
+//Topic Cara Benar Menggunakan Fungsi Promise
+// Promise version
+// const requestPromise = (url) => {
+//   return new Promise((resolve, reject) => {
+//     const delay = Math.floor(Math.random() * 4500) + 500;
+//     setTimeout(() => {
+//       if (delay > 3000) {
+//         reject("Error: Connection Timeout");
+//       } else {
+//         resolve(`Success: ${url} (${delay}ms)`);
+//       }
+//     }, delay);
+//   });
+// };
+
+// requestPromise("movie.com")
+//   .then((result) => {
+//     console.log("page 1");
+//     console.log(result);
+//     return requestPromise("movie.com");
+//   })
+//   .then(() => {
+//     console.log("page 2");
+//     return requestPromise("movie.com");
+//   })
+//   .then(() => {
+//     console.log("page 3");
+//     return requestPromise("movie.com");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
